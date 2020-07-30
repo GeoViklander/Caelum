2b76fa194ab85e0df7d17cfb7429b9951384a377
gpesantos <gpesantos@recordtv.com.br>
2020-07-30 00:02:48 -0300

    de novo

---
+++ wd43-introducao/comportamento.js
@@ -0,0 +1,15 @@
+/*
+Javascript - Linguagem de programação
+// 2 Camadas
+// Armazenamento 
+
+var chave = "valor";
+
+// Instruções
+
+comando();
+comando(parametro);
+*/
+
+var mensagem ="<br>Boa noite!"; //armazenado na memoria 
+document.write(mensagem); //irá aparecer na tela
